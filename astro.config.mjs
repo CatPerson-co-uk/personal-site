7e0def7d3c40430563ae6ad51b7af4f712b01ca9
catpersonuk <gitlab.blitz406@passinbox.com>
2024-11-01 21:18:30 +0000

    added nextui

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,13 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [tailwind(), react()]
 });
